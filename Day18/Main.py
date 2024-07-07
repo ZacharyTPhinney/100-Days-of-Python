c307f5c0e37a012e263964ca02b33c27f4597c07
ZacharyTPhinney <zacharytphinney@Protonmail.com>
2024-07-07 16:07:18 -0400

    Add files via upload

--- Day18/Main.py
+++ Day18/Main.py
@@ -2,19 +2,24 @@ import turtle as t
 import random
 
 tim = t.Turtle()
-
+t.colormode(255)
+def random_color():
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  return r,g,b
 ########### Challenge 4 - Random Walk ########
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 walk=True
 while walk==True:
   i = random.randint(0,2)
 
-  for j in range(0,100):
-    if i ==0:
-      tim.right(j)
-      tim.forward(50)
-      tim.color(random.choice(colours))
-    elif i==1:
-      tim.color(random.choice(colours))
-      tim.left(j)
-      tim.forward(10)
+  tim.pensize(10)
+  if i ==0:
+    tim.right(45)
+    tim.forward(10)
+    tim.color(random_color())
+  elif i==1:
+    tim.color(random_color())
+    tim.left(45)
+    tim.forward(10)
